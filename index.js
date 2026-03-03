570390d87218d8c6773ac413e0001e91b60c042f
worthamjh <worthamjh@gmail.com>
2026-03-03 10:25:55 -0600

    Add method-override and CRUD routes for comments
    Integrate method-override to support PATCH/DELETE semantics from HTML forms. Require and enable middleware (app.use(methodOverride('_method'))) and switch the comments array to let so it can be reassigned on delete. Add routes and views support: GET /comments/new, GET /comments/:id/edit, and DELETE /comments/:id (existing POST/PATCH flows remain). Update comment show/edit templates to use the override method and update package.json to include the method-override dependency.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path= require('path');
+const methodOverride = require('method-override')
 const {v4: uuid } = require('uuid');
 
-const comments = [
+let comments = [
   { id: uuid(),
     username: "CodeMaster21", 
     comment: "Just finished my first JavaScript project!"
@@ -47,6 +48,7 @@ const comments = [
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -65,24 +67,40 @@ app.post('/tacos', (req,res) => {
 app.get('/comments', (req,res) => {
     res.render('comments/index.ejs', {comments})
 })
+
 app.get('/comments/new', (req,res) => {
     res.render('comments/new')
 })
+
 app.post('/comments', (req,res) => {
     const {username, comment} = req.body;
     comments.push({username, comment, id: uuid()})
     res.redirect('/comments');
 })
+
 app.get('/comments/:id', (req,res) => {
   const { id } = req.params;
   const comment = comments.find(c => c.id === id);
   res.render('comments/show', {comment})
 })
+
+app.get('/comments/:id/edit', (req,res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/edit', {comment})
+})
+
 app.patch('/comments/:id', (req,res) => {
   const { id } = req.params;
   const newCommentText=req.body.comment;
   const foundComment = comments.find(c => c.id === id);
   foundComment.comment= newCommentText;
+  res.redirect('/comments');
+})
+
+app.delete('/comments/:id', (req,res) => {
+  const { id } = req.params;
+  comments = comments.filter(c => c.id !==id);
   res.redirect('/comments');
 })
 
